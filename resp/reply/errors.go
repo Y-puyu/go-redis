c3d59edae7d5ac61ebdba4288a8dfee341e7a1b9
Ypuyu <345462072@qq.com>
2025-05-27 22:20:49 +0800

    [Add] 实现 error reply

---
+++ resp/reply/errors.go
@@ -0,0 +1,91 @@
+package reply
+
+// UnknownErrReply represents UnknownErr
+type UnknownErrReply struct{}
+
+// 未知错误的回复
+var unknownErrBytes = []byte("-Err unknown\r\n")
+
+// ToBytes marshals redis.Reply
+func (r *UnknownErrReply) ToBytes() []byte {
+	return unknownErrBytes
+}
+
+func (r *UnknownErrReply) Error() string {
+	return "Err unknown"
+}
+
+// ArgNumErrReply represents wrong number of arguments for command
+// 参数错误的回复
+type ArgNumErrReply struct {
+	Cmd string // 返回命令本身
+}
+
+// ToBytes marshals redis.Reply
+func (r *ArgNumErrReply) ToBytes() []byte {
+	return []byte("-ERR wrong number of arguments for '" + r.Cmd + "' command\r\n")
+}
+
+func (r *ArgNumErrReply) Error() string {
+	return "ERR wrong number of arguments for '" + r.Cmd + "' command"
+}
+
+// MakeArgNumErrReply represents wrong number of arguments for command
+func MakeArgNumErrReply(cmd string) *ArgNumErrReply {
+	return &ArgNumErrReply{
+		Cmd: cmd,
+	}
+}
+
+// SyntaxErrReply represents meeting unexpected arguments
+type SyntaxErrReply struct{}
+
+// 语法错误
+var syntaxErrBytes = []byte("-Err syntax error\r\n")
+var theSyntaxErrReply = &SyntaxErrReply{}
+
+// MakeSyntaxErrReply creates syntax error
+func MakeSyntaxErrReply() *SyntaxErrReply {
+	return theSyntaxErrReply
+}
+
+// ToBytes marshals redis.Reply
+func (r *SyntaxErrReply) ToBytes() []byte {
+	return syntaxErrBytes
+}
+
+func (r *SyntaxErrReply) Error() string {
+	return "Err syntax error"
+}
+
+// WrongTypeErrReply represents operation against a key holding the wrong kind of value
+type WrongTypeErrReply struct{}
+
+// 错误的数据类型
+var wrongTypeErrBytes = []byte("-WRONGTYPE Operation against a key holding the wrong kind of value\r\n")
+
+// ToBytes marshals redis.Reply
+func (r *WrongTypeErrReply) ToBytes() []byte {
+	return wrongTypeErrBytes
+}
+
+func (r *WrongTypeErrReply) Error() string {
+	return "WRONGTYPE Operation against a key holding the wrong kind of value"
+}
+
+// ProtocolErr
+
+// ProtocolErrReply represents meeting unexpected byte during parse requests
+// 接口协议错误。客户端指令不符合 RESP 规范
+type ProtocolErrReply struct {
+	Msg string
+}
+
+// ToBytes marshals redis.Reply
+func (r *ProtocolErrReply) ToBytes() []byte {
+	return []byte("-ERR Protocol error: '" + r.Msg + "'\r\n")
+}
+
+func (r *ProtocolErrReply) Error() string {
+	return "ERR Protocol error: '" + r.Msg
+}
